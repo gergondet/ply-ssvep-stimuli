45c7cb5504de55934b280982f1daf6fcb49db64e
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2014-01-14 20:00:16 +0900

    Fix threading issue
    It seems InitOculus and OculusDisplayLoop have to be called in the same thread O-o

--- test/testply-ssvep-stimuli.cpp
+++ test/testply-ssvep-stimuli.cpp
@@ -12,9 +12,30 @@
 #include <iostream>
 #include <libgen.h>
 
-void update(bciinterface::PLYObject * object, sf::Clock & anim_clock)
+int main(int argc, char * argv[])
 {
-    while(1)
+    unsigned int width = 1280;
+    unsigned int height = 800;
+
+    bciinterface::BCIInterface iface(width, height);
+
+    std::string model = std::string(dirname(argv[0])) + "/models/can.ply";
+    bciinterface::PLYObject * object = new bciinterface::PLYObject(model);
+
+    bciinterface::FontManager fm;
+    bciinterface::FPSCounter fps_c(fm.GetDefaultFont());
+    iface.AddNonOwnedObject(&fps_c);
+
+    sf::Clock anim_clock;
+
+    int cmd = -1;
+    boost::thread th2( boost::bind(&bciinterface::BCIInterface::OculusDisplayLoop, &iface, boost::ref(cmd), "/home/gergondet/devel/share/OculusWindow") );
+
+    sleep(1);
+    object->RegisterWithOculus(iface.GetOculusWindow());
+    iface.AddObject(object);
+    iface.StartParadigm();
+    while(iface.ParadigmStatus())
     {
         static float angle = 0;
         glm::mat4 anim_box = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0, 1, 0));
@@ -24,32 +45,7 @@ void update(bciinterface::PLYObject * object, sf::Clock & anim_clock)
         model_box = model_box*anim_box;
         object->SetModel(model_box);
     }
-}
-
-int main(int argc, char * argv[])
-{
-    unsigned int width = 1280;
-    unsigned int height = 800;
-
-    bciinterface::BCIInterface iface(width, height);
-    iface.InitOculus("/home/gergondet/devel/share/OculusWindow");
-
-    std::string model = std::string(dirname(argv[0])) + "/models/can.ply";
-    bciinterface::PLYObject * object = new bciinterface::PLYObject(model);
-    object->RegisterWithOculus(iface.GetOculusWindow());
-    iface.AddObject(object);
-
-    bciinterface::FontManager fm;
-    bciinterface::FPSCounter fps_c(fm.GetDefaultFont());
-    iface.AddNonOwnedObject(&fps_c);
-
-    sf::Clock anim_clock;
-    boost::thread th( boost::bind(&update, object, boost::ref(anim_clock)) );
-
-    int cmd = -1;
-    iface.OculusDisplayLoop(cmd);
-//    boost::thread th2( boost::bind(&bciinterface::BCIInterface::OculusDisplayLoop, &iface, boost::ref(cmd), "") );
-//    th2.join();
+    th2.join();
 
     return 0;
 }
